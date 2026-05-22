b3d63ad0cede5c76da4ee53159582a876509eb4c
NicolaSarmiento25 <sarmiento.02.35@gmail.com>
2026-05-22 11:52:11 -0500

    Se elimino reportes y se corrigio routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,17 +4,10 @@ export const routes: Routes = [
   { path: '', redirectTo: 'registro', pathMatch: 'full' },
   {
     path: 'registro',
-    // Ajusta la ruta según el nombre real de tu archivo:
-    // Si se llama registro.ts        -> './registro'
-    // Si se llama registro.component.ts -> './registro.component'
     loadComponent: () => import('./registro/registro').then(m => m.Registro)
   },
   {
     path: 'consulta',
     loadComponent: () => import('./consulta/consulta').then(m => m.Consulta)
   },
-  {
-    path: 'reportes',
-    loadComponent: () => import('./reportes/reportes').then(m => m.ReportesComponent)
-  }
 ];
